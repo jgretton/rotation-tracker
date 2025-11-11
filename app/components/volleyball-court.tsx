a9f6535e961e125a1a89399f80124d1032119d11
Joshua Gretton <jb.gretton@googlemail.com>
2025-11-11 15:38:18 +0000

    Update volleyballCourt
    Added aspect ratio and updated styles for mobile.

--- app/components/volleyball-court.tsx
+++ app/components/volleyball-court.tsx
@@ -6,20 +6,21 @@ export default function VolleyballCourt({
 	return (
 		<div
 			id="half-court-boundaries"
-			className="w-[1000px] h-[800px] bg-[#0873A7] relative"
+			// className="w-[1000px] h-[800px] bg-[#0873A7] relative"
+			className="max-w-7xl w-full aspect-5/4 bg-[#0873A7] relative"
 		>
 			<div
 				id="net"
-				className=" w-4/6 h-8 rounded-md bg-gray-600 border border-black mx-auto"
+				className=" w-4/6  h-4 md:h-8 rounded-md bg-gray-600 border border-black mx-auto"
 			/>
 
 			<div
 				id="half-court"
-				className="w-3/5 h-4/5 bg-[#FF915C] mx-auto relative border-white border-8 border-t-0"
+				className="w-3/5 h-4/5 bg-[#FF915C] mx-auto relative border-white border-4 md:border-8 border-t-0 md:border-t-0"
 			>
 				<div
 					id="3m-line"
-					className="absolute w-full h-1/3 border-b-4 border-white"
+					className="absolute w-full h-1/3 border-b-2 md:border-b-4 border-white"
 				/>
 
 				{children}
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
 		<div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans">
 			<h1>Rotation Tracker</h1>
 			<VolleyballCourt>
-				<div className="size-full grid grid-cols-3 grid-rows-2 gap-5 p-5">
-					<div className="size-full border border-white"></div>
-					<div className="size-full border border-white"></div>
-					<div className="size-full border border-white"></div>
-					<div className="size-full border border-white"></div>
-					<div className="size-full border border-white"></div>
-					<div className="size-full border border-white"></div>
+				<div className="size-full grid grid-cols-3 grid-rows-2 md:gap-5 gap-2 p-5">
+					<div className="size-full border border-white bg-white/20"></div>
+					<div className="size-full border border-white bg-white/20"></div>
+					<div className="size-full border border-white bg-white/20"></div>
+					<div className="size-full border border-white bg-white/20"></div>
+					<div className="size-full border border-white bg-white/20"></div>
+					<div className="size-full border border-white  bg-white/20"></div>
 				</div>
 			</VolleyballCourt>
 		</div>
