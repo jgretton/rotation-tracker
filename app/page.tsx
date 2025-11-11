a6955ef265bd23cbab3819102da48d4319955b8a
Joshua Gretton <jb.gretton@googlemail.com>
2025-11-11 15:27:07 +0000

    Volleyball court structure
    Basic structure for volleyball court added. Fixed height and width
    still, need to change this but for demo purposes its fine.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import VolleyballCourt from "./components/volleyball-court";
 
 export default function Home() {
 	return (
-		<div className="">
+		<div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans">
 			<h1>Rotation Tracker</h1>
+			<VolleyballCourt>
+				<div className="size-full grid grid-cols-3 grid-rows-2 gap-5 p-5">
+					<div className="size-full border border-white"></div>
+					<div className="size-full border border-white"></div>
+					<div className="size-full border border-white"></div>
+					<div className="size-full border border-white"></div>
+					<div className="size-full border border-white"></div>
+					<div className="size-full border border-white"></div>
+				</div>
+			</VolleyballCourt>
 		</div>
 	);
 }
